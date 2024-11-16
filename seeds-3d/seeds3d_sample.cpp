6a38cc6bfc306641369ff8602d1ea4a8549eb1d3
farbeyondjy <jiangyan0309@126.com>
2024-11-17 01:29:28 +0800

    add idx bar

--- seeds-3d/seeds3d_sample.cpp
+++ seeds-3d/seeds3d_sample.cpp
@@ -32,7 +32,7 @@ int main()
     int depth = 48;
     int height = 192;
     int width = 192;
-    std::ifstream file("/Users/Zach/Zch/Research/seeds-3d/seeds-3d/array_3d.bin", std::ios::binary);
+    std::ifstream file("/Users/jiangyan/Desktop/seeds-3d-master/seeds-3d/array_3d.bin", std::ios::binary);
     if (!file.is_open())
     {
         std::cerr << "Error opening file!" << std::endl;
@@ -43,14 +43,6 @@ int main()
     file.close();
     Mat img_data = Mat(3, (int[]){depth, height, width}, CV_32FC1, data.data());
     Mat frame(height, width, CV_8UC1);
-    int idx = 20;
-    for (int i = 0; i < height; ++i)
-    {
-        for (int j = 0; j < width; ++j)
-        {
-            frame.at<uchar>(i, j) = static_cast<uchar>(data[idx * width * height + i * height + j]);
-        }
-    }
 
     namedWindow(window_name, 0);
     int num_iterations = 4;
@@ -59,6 +51,8 @@ int main()
     int num_superpixels = 400;
     int num_levels = 4;
     int num_histogram_bins = 5;
+    int idx = 20;
+    createTrackbar("idx of image", window_name, &idx, 48, trackbarChanged);
     createTrackbar("Number of Superpixels", window_name, &num_superpixels, 1000, trackbarChanged);
     createTrackbar("Smoothing Prior", window_name, &prior, 5, trackbarChanged);
     createTrackbar("Number of Levels", window_name, &num_levels, 10, trackbarChanged);
@@ -70,13 +64,20 @@ int main()
 
     for (;;)
     {
-        Mat result = frame.clone();
         if (!init)
         {
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    frame.at<uchar>(i, j) = static_cast<uchar>(data[idx * width * height + i * height + j]);
+                }
+            }
             seeds = createSuperpixelSEEDS3D(width, height, depth, 1, num_superpixels,
                                             num_levels, prior, num_histogram_bins, double_step);
             init = true;
         }
+        Mat result = frame.clone();
 
         double t = (double)getTickCount();
 
