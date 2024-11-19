15b9be53a1a6fd6cfbaf15e22689cd2289fcdaf3
Zch0414 <njuzch0414@gmail.com>
2024-11-19 09:11:05 -0500

    Compatible with Linux

--- samples/seeds3d-cpp/seeds3d_sample.cpp
+++ samples/seeds3d-cpp/seeds3d_sample.cpp
@@ -41,7 +41,8 @@ int main()
     std::vector<float> data(depth * height * width);
     file.read(reinterpret_cast<char *>(data.data()), depth * height * width * sizeof(float));
     file.close();
-    Mat img_data = Mat(3, (int[]){depth, height, width}, CV_32FC1, data.data());
+    int img_sizes = {depth, height, width};
+    Mat img_data = Mat(3, img_sizes, CV_32FC1, data.data());
     Mat frame(height, width, CV_8UC1);
 
     namedWindow(window_name, 0);
