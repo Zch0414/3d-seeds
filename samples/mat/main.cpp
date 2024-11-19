03b569c8afe825f8a3d8da56bb5a975686ff91cb
Zch0414 <njuzch0414@gmail.com>
2024-11-19 09:10:29 -0500

    Compatible with Linux

--- samples/mat/main.cpp
+++ samples/mat/main.cpp
@@ -5,7 +5,8 @@ int main() {
     // Dimensions of the 3D matrix
     cv::Mat mat3D;
     // Create a 3D Mat with a single channel of 32-bit integer type
-    mat3D = cv::Mat(3, (int[]){2, 4, 4}, CV_32SC1);
+    int sizes[3] = {2, 4, 4};
+    mat3D = cv::Mat(3, sizes, CV_32SC1);
     // Fill the matrix with values 0 to 26
     for (int i = 0; i < 32; ++i) {
         mat3D.data[i * sizeof(int)] = i;  // Populate each element in the 3D Mat
